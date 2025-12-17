6859d155d86bb5045304b8911436b46ba45cf0a9
aryanverma2601 <aryanv380@gmail.com>
2025-12-17 18:11:53 +0530

    Implementing basic programs
    Completed 5 ques of java programming

---
+++ core-java-practice/leet-code-codebase/Largest_Two_Number.java
@@ -0,0 +1,11 @@
+class LargestNumber {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+
+        if (a > b)
+            System.out.println("A is largest");
+        else
+            System.out.println("B is largest");
+    }
+}
